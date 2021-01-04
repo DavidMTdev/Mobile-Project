569de1fe5c21a0fdefab9dd20a41651bf16ffa5e
DavidMTdev <david.teixeira31@gmail.com>
2021-01-04 11:49:13 +0100

    Update show data list

--- app/src/main/java/fr/ynov/sycker/utils/Constant.java
+++ app/src/main/java/fr/ynov/sycker/utils/Constant.java
@@ -1,4 +1,5 @@
 package fr.ynov.sycker.utils;
 
 public class Constant {
+    public static final String URL = "https://opendata.paris.fr/api/records/1.0/search/?dataset=coronavirus-commercants-parisiens-livraison-a-domicile&q=";
 }
--- app/src/main/java/fr/ynov/sycker/view/DataDetails.java
+++ app/src/main/java/fr/ynov/sycker/view/DataDetails.java
@@ -1,15 +1,25 @@
 package fr.ynov.sycker.view;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import fr.ynov.sycker.AppActivity;
 
 import fr.ynov.sycker.R;
+import fr.ynov.sycker.models.merchant.Fields;
 
 public class DataDetails extends AppActivity {
+    private Fields merchant;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_list);
+        setContentView(R.layout.activity_data_details);
+
+        if(getIntent().getExtras() != null) {
+            merchant = (Fields) getIntent().getExtras().get("merchant");
+
+            Log.e("merchant", merchant.getNom_du_commerce());
+        }
     }
 }
