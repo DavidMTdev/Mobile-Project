8dff4c87b351ff96682e3490441c0304f9041223
DavidMTdev <david.teixeira31@gmail.com>
2021-01-04 10:56:01 +0100

    Add DataList Activity

--- app/src/main/java/fr/ynov/sycker/view/DataList.java
+++ app/src/main/java/fr/ynov/sycker/view/DataList.java
@@ -1,14 +1,18 @@
-package fr.ynov.sycker;
+package fr.ynov.sycker.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import fr.ynov.sycker.AppActivity;
+
+import fr.ynov.sycker.R;
+
+public class DataList extends AppActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_data_list);
     }
 }
